f1dc7ee38c567ac234cade68bddaf6ecd589bbd5
Paul Franosch <paul@franos.ch>
2026-02-09 20:36:14 +0100

    Add openapi doc

--- src/main/java/ch/ethz/eyetap/configuration/SecurityConfig.java
+++ src/main/java/ch/ethz/eyetap/configuration/SecurityConfig.java
@@ -34,6 +34,9 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/swagger-ui.html").permitAll()
+                .requestMatchers("/swagger-ui/**").permitAll()
                 .anyRequest().authenticated()
         );
 
