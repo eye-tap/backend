0bc32f42857f52f9c8df08c866d99439e6afa062
Paul Franosch <paul@franos.ch>
2026-07-09 22:24:26 +0200

    Generate annotation session for on the fly generated participants

--- src/main/java/ch/ethz/eyetap/controller/MagicLinkGeneratorController.java
+++ src/main/java/ch/ethz/eyetap/controller/MagicLinkGeneratorController.java
@@ -3,16 +3,20 @@ package ch.ethz.eyetap.controller;
 
 import ch.ethz.eyetap.dto.CreateSurveyDto;
 import ch.ethz.eyetap.model.User;
+import ch.ethz.eyetap.model.annotation.AnnotationSession;
+import ch.ethz.eyetap.model.annotation.ReadingSession;
 import ch.ethz.eyetap.model.survey.Survey;
+import ch.ethz.eyetap.repository.AnnotationSessionRepository;
+import ch.ethz.eyetap.repository.ReadingSessionRepository;
 import ch.ethz.eyetap.repository.SurveyRepository;
 import ch.ethz.eyetap.repository.UserRepository;
+import ch.ethz.eyetap.service.AnnotationSessionService;
 import ch.ethz.eyetap.service.AuthService;
 import ch.ethz.eyetap.service.SurveyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
@@ -24,10 +28,15 @@ public class MagicLinkGeneratorController {
     private final AuthService authService;
     private final SurveyRepository surveyRepository;
     private final UserRepository userRepository;
+    private final ReadingSessionRepository readingSessionRepository;
+    private final AnnotationSessionService annotationSessionService;
+    private final AnnotationSessionRepository annotationSessionRepository;
 
     // todo: this is insecure as of now, fix later
     @PostMapping("/draw/{surveyId}")
-    public AuthService.SurveyParticipant draw(@PathVariable Long surveyId) {
+    public AuthService.SurveyParticipant draw(
+            @RequestBody Set<Long> readingSessionIds,
+            @PathVariable Long surveyId) {
         Survey survey = this.surveyService.getById(surveyId);
         List<String> pseudonyms = this.surveyService.generateFreshPseudonyms(1);
         List<AuthService.SurveyParticipant> surveyParticipantsBatch = this.authService.createSurveyParticipantsBatch(pseudonyms);
@@ -36,7 +45,20 @@ public class MagicLinkGeneratorController {
         survey.getUsers().add(participant.user());
         participant.user().setSurveys(Set.of(survey));
 
-        this.userRepository.save(participant.user());
+        User user = this.userRepository.save(participant.user());
+        survey = this.surveyRepository.save(survey);
+
+        Set<AnnotationSession> annotationSessions = new HashSet<>();
+
+        for (Long readingSessionId : readingSessionIds) {
+            ReadingSession readingSession = this.readingSessionRepository.findById(readingSessionId)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Reading session with id " + readingSessionId + " not found."));
+            AnnotationSession initialize = this.annotationSessionService.initialize(survey, user, readingSession);
+            annotationSessions.add(initialize);
+        }
+
+        survey.setAnnotationSessions(annotationSessions);
+        this.annotationSessionRepository.saveAll(annotationSessions);
         this.surveyRepository.save(survey);
 
         return participant;
